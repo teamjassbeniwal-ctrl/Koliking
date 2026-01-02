f020f4f33c9c3634afe3a4af9e84f9d737ba8c07
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-01-02 16:39:15 +0530

    Update main.py

--- devgagan/modules/main.py
+++ devgagan/modules/main.py
@@ -228,11 +228,11 @@ async def batch_link(_, message):
         await message.reply(response_message)
         return
         
-    join_button = InlineKeyboardButton("Join Channel", url="https://t.me/+pZZQnMwRZR8yMTZl")
+    join_button = InlineKeyboardButton("Join Channel", url="https://t.me/teamjb1")
     keyboard = InlineKeyboardMarkup([[join_button]])
     pin_msg = await app.send_message(
         user_id,
-        f"Batch process started ⚡\nProcessing: 0/{cl}\n\n**Powered By ᴛᴇᴀᴍ  ꫝຮ 亗**",
+        f"Batch process started ⚡\nProcessing: 0/{cl}\n\n**Powered By ᴛeam JB**",
         reply_markup=keyboard
     )
     await pin_msg.pin(both_sides=True)
@@ -251,14 +251,14 @@ async def batch_link(_, message):
                     msg = await app.send_message(message.chat.id, f"Processing...")
                     await process_and_upload_link(userbot, user_id, msg.id, link, 0, message)
                     await pin_msg.edit_text(
-                        f"Batch process started ⚡\nProcessing: {i - cs + 1}/{cl}\n\n**__Powered By ᴛᴇᴀᴍ  ꫝຮ 亗__**",
+                        f"Batch process started ⚡\nProcessing: {i - cs + 1}/{cl}\n\n**__Powered By ᴛeam JB__**",
                         reply_markup=keyboard
                     )
                     normal_links_handled = True
         if normal_links_handled:
             await set_interval(user_id, interval_minutes=300)
             await pin_msg.edit_text(
-                f"Batch completed successfully for {cl} messages 🎉\n\n**__Powered By ᴛᴇᴀᴍ  ꫝຮ 亗__**",
+                f"Batch completed successfully for {cl} messages 🎉\n\n**__Powered By ᴛᴇᴀᴍ  JB__**",
                 reply_markup=keyboard
             )
             await app.send_message(message.chat.id, "😘 𝗖ꪮ𝗺𝗽𝗹𝗲𝘁𝗲 𝗛ꪮ 𝗚𝗮𝘆𝗮 𝗕ꪮ$$ 😎")
@@ -283,7 +283,7 @@ async def batch_link(_, message):
 
         await set_interval(user_id, interval_minutes=300)
         await pin_msg.edit_text(
-            f"Batch completed successfully for {cl} messages 🎉\n\n**__Powered By ᴛᴇᴀᴍ  ꫝຮ 亗__**",
+            f"Batch completed successfully for {cl} messages 🎉\n\n**__Powered By ᴛᴇᴀᴍ  JB__**",
             reply_markup=keyboard
         )
         await app.send_message(message.chat.id, "Batch completed successfully! 🎉")
