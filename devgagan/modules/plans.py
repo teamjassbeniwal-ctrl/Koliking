26b1803f8b1c39b315bb40a047f8cfc30c5cafb3
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-01-02 16:33:30 +0530

    Update plans.py

--- devgagan/modules/plans.py
+++ devgagan/modules/plans.py
@@ -160,7 +160,7 @@ async def transfer_premium(client, message):
                 f"👤 **From:** {sender_user.mention}\n"
                 f"👤 **To:** {new_user.mention}\n"
                 f"⏳ **Expiry Date:** {expiry_str_in_ist}\n\n"
-                f"__Powered By ᴛᴇᴀᴍ  ꫝຮ 亗__ 🚀"
+                f"__Powered By Team JB__ 🚀"
             )
             
             # Notification to the new user
