a7bf30a75223c7626fd802609cca5c4d52f7262e
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-03-11 16:20:25 +0530

    Update ytdl.py

--- devgagan/modules/ytdl.py
+++ devgagan/modules/ytdl.py
@@ -624,8 +624,8 @@ async def process_video(client, message, url, cookies_env_var, check_duration):
     # yt-dlp options
     ydl_opts = {
     'outtmpl': download_path,
-    'format': 'bestvideo*+bestaudio/best'  # keep this for video+aud
-    'merge_output_format': 'mp4',   
+    'format': 'bestvideo*+bestaudio/best'
+    'merge_output_format': 'mp4',   # keep this for video+aud  
     'cookiefile': '/app/cookies/youtube.txt',
 
     'writethumbnail': True,
