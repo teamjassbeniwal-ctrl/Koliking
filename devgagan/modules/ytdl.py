f38f593f95673787c0e23faf5b870e8953608fb8
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-03-12 08:55:02 +0530

    Update ytdl.py

--- devgagan/modules/ytdl.py
+++ devgagan/modules/ytdl.py
@@ -239,17 +239,11 @@ async def process_audio(client: Client, message: Message, url: str, cookies_env_
     'quiet': False,
     'noplaylist': True,
     'js_runtimes': {'node': {}},
-    'remote_components': ['ejs:github'],
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    'extractor_args': {
-        'youtube': {
-            'player_client': ['android', 'web']
-        }
-    },
     'http_headers': {
         'User-Agent': 'Mozilla/5.0'
     }
@@ -429,10 +423,6 @@ async def process_video(client, message, url, cookies_env_var, check_duration):
         'verbose': True,
         'noplaylist': True,
         'js_runtimes': {'node': {}},
-        'remote_components': ['ejs:github'],
-        'extractor_args': {
-            'youtube': {'player_client': ['android', 'web']}
-        },
         'http_headers': {'User-Agent': 'Mozilla/5.0'}
     }
 
